a90171a0ebe4e42a99dbcda08b09c01975d7e2f2
SungYinYang <54567577+SungYinYang@users.noreply.github.com>
2020-04-13 02:08:00 -0700

    check validate

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# python-finance
+# Python-Finance-Info-Web-App
+
+The web application can be accessed at : [Finance Info](https://python-homework2.herokuapp.com)
+
+Used IexFinance API to retrieve financial data.
+
+The web app is written with Python and will take a stock symbol as input and output the following:
+- Current date and time
+- Full name of the company
+- Stock price
+- Value changes (+ for increase and - for decrease)
+- Percentage changes (+ for increase and - for decrease)
+
+The web app also handles some error situations such as no network, invalid symbol, etc.
+
+
+## For example:
+
+#### Input:
+
+Please enter a symbol:
+
+ADBE
+
+#### Output:
+
+Mon Oct 10 17:23:48 PDT 2016
+
+Adobe Systems Incorporated (ADBE)
+
+109.24 +0.60 (+0.55%)
--- app.py
+++ app.py
@@ -1,99 +1,54 @@
 from flask import Flask, request
-import yfinance as yf
-import datetime
+from iexfinance import Stock
+import iexfinance.utils.exceptions as excep
 import time
-
 
 now = time.strftime("%c")
 
 app = Flask(__name__)
 
-def print_stock_price(history):
-    symbol=''
-    if(history[1] - history[0] >=0):
-        symbol = '+'
-    priceDiff = history[1] - history[0]
-    pricePercent = priceDiff/history[0]
-    return ( str(round(history[1], 2)) +" "+ symbol+ str(round(priceDiff, 2))+" ("+symbol+ str(("%.2f" % round(pricePercent, 2))+"%)"))
-
-def get_day_of_week(num):
-    switcher = {
-        1: "Mon",
-        2: "Tue",
-        3: "Wed",
-        4: "Thu",
-        5: "Fri",
-        6: "Sat",
-        7: "Sun"
-    }
-    return switcher.get(num)
-
-def get_month(num):
-    switcher = {
-        1: "January",
-        2: "February",
-        3: "March",
-        4: "April",
-        5: "May",
-        6: "June",
-        7: "July",
-        8: "August",
-        9: "September",
-        10: "October",
-        11: "November",
-        12: "December"
-    }
-    return switcher.get(num)
-
-def print_time():
-    cur = datetime.datetime.now()
-
-    day_of_week = get_day_of_week(cur.isoweekday())
-    month = get_month(cur.month)
-    date = str(cur.day)
-    time = '{date:%H:%M:%S}'.format( date=datetime.datetime.now())
-    timeZone= 'PDT'
-    year = str(cur.year)
-    #print(day_of_week, month, date, time, timeZone, year)
-    return (day_of_week+" "+month+" "+date+" "+time+" "+timeZone+" "+year)
-
-def print_name(information):
-    return (information['longName']+ "(" + information['symbol']+")")
-
 @app.route('/', methods=['GET', 'POST']) #allow both GET and POST requests
 def index():
     if request.method == 'POST':  #this block is only entered when the form is submitted
+        stock = request.form['symbol']
+        symbol = Stock(stock)
 
-        try:
-            symbol = request.form['symbol']
-            msft = yf.Ticker(symbol)
-            hist = msft.history(period="2d")
+        name = symbol.get_company_name() + " (" + str(stock) +  ")\n\n"
+        curr_time = now
+        price = str(symbol.get_price()) + " " + str(symbol.get_quote().get('change')) + " (" + str(symbol.get_quote().get('changePercent')) + ")"
 
-            time = print_time()
-            name = print_name(msft.info)
-            price = print_stock_price(hist.Close)
-
-        except:
-            name="there is an error in the input"
-            curr_time=" yFinance do not have this stock's information."
-            price = " Please try another valid input such as 'msft', 'aapl' or 'googl'"
-
-        return '''<title>Python Finance Info - Individual Homework</title>
+        return '''<title>Python Finance Info</title>
             <form method="POST">
-                <h3>Python Finance Info - Individual Homework</h3><br/>
-                <i>Input:</i><br/><br/>
-                Enter a stock symbol <input type="text" name="symbol">
+                <h3>Python Finance Info</h3><br/>
+                <i>Input:</i><br/><br/> 
+                Enter a symbol <input type="text" name="symbol">
                 <input type="submit" value="Submit"><br/><br/>
             </form>
-            <i>Output: </i><br/><br/> {} <br/> {} <br/> {} </h5>'''.format(name, time, price)
+            <i>Output: </i><br/><br/> {} <br/> {} <br/> {} </h5>'''.format(name, curr_time, price)
 
-    return '''<title>Python Finance Info - Individual Homework</title>
+    return '''<title>Python Finance Info</title>
         <form method="POST">
             <h3>Python Finance Info</h3><br/>
             <i> Input: </i><br/><br/>
-            Enter a stock symbol: <input type="text" name="symbol"><br/>
+            Enter a symbol: <input type="text" name="symbol"><br/>
             <input type="submit" value="Submit"><br/>
         </form>'''
+
+@app.errorhandler(excep.IEXSymbolError)
+def symbol_error_handle(e):
+    return '''{} Please enter a valid ticker symbol!'''.format(e)
+
+@app.errorhandler(excep.IEXEndpointError)
+def endpoint_error_handle(e):
+    return '''{}'''.format(e)
+
+@app.errorhandler(excep.IEXFieldError)
+def field_error_handle(e):
+    return '''{}'''.format(e)
+
+@app.errorhandler(excep.IEXQueryError)
+def query_error_handle(e):
+    return '''{}'''.format(e)
 
 @app.errorhandler(AttributeError)
 def attribute_error_handle(e):
